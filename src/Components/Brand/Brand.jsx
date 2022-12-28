5391635f26b1e99965bca26ccb9c91b52438a22f
Lehho021 <lehho011121@gmail.com>
2022-12-28 10:02:08 +0700

    menambahkan logo brand

--- src/Components/Brand/Brand.jsx
+++ src/Components/Brand/Brand.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect } from "react";
 import "./brand.css";
 
-import Brand1 from "../../Assets/brand1.png";
-import Brand2 from "../../Assets/brand2.png";
-import Brand3 from "../../Assets/brand3.png";
-import Brand4 from "../../Assets/brand4.png";
-import Brand5 from "../../Assets/brand5.png";
-import Brand6 from "../../Assets/brand6.png";
+import Brand1 from "../../Assets/Brand1.png";
+import Brand2 from "../../Assets/Brand2.png";
+import Brand3 from "../../Assets/Brand3.png";
+import Brand4 from "../../Assets/Brand4.png";
+import Brand5 from "../../Assets/Brand5.png";
+import Brand6 from "../../Assets/Brand6.png";
+import Brand7 from "../../Assets/Brand7.png";
+import Brand8 from "../../Assets/Brand8.png";
+import Brand9 from "../../Assets/Brand9.png";
+import Brand10 from "../../Assets/Brand10.png";
+import Brand11 from "../../Assets/Brand11.png";
+import Brand12 from "../../Assets/Brand12.png";
 
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -20,12 +26,12 @@ const Brand = () => {
     <section className="brand container section">
       <div className="secContainer">
         <div data-aos="fade-up" data-aos-duration="2000" className="secIntro">
-          <h2 className="secTitle">OFFICIAL SPONSORS</h2>
+          <h2 className="secTitle">SPONSORS & PARTNERS</h2>
         </div>
 
         <div className="mainContent grid">
           <div className="brandImage">
-            <p>Official Beer Partner</p>
+            <p>Official Partner</p>
             <img src={Brand1} alt="" />
           </div>
         </div>
@@ -62,6 +68,36 @@ const Brand = () => {
             <div className="imgDiv">
               <div className="titleArtis">
                 <img src={Brand6} alt="artis" />
+              </div>
+            </div>
+            <div className="imgDiv">
+              <div className="titleArtis">
+                <img src={Brand7} alt="artis" />
+              </div>
+            </div>
+            <div className="imgDiv">
+              <div className="titleArtis">
+                <img src={Brand8} alt="artis" />
+              </div>
+            </div>
+            <div className="imgDiv">
+              <div className="titleArtis">
+                <img src={Brand9} alt="artis" />
+              </div>
+            </div>
+            <div className="imgDiv">
+              <div className="titleArtis">
+                <img src={Brand10} alt="artis" />
+              </div>
+            </div>
+            <div className="imgDiv">
+              <div className="titleArtis">
+                <img src={Brand11} alt="artis" />
+              </div>
+            </div>
+            <div className="imgDiv">
+              <div className="titleArtis">
+                <img src={Brand12} alt="artis" />
               </div>
             </div>
           </div>
